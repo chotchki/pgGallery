2fbea80261336675005e178c0d5a2de212ebe552
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2011-07-06 21:57:56 -0400

    Got the landing page working

--- jsafe/src/main/java/jsafe/web/MainController.java
+++ jsafe/src/main/java/jsafe/web/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/index")
 	public String showIndexPage(){
 		return "index";
 	}
