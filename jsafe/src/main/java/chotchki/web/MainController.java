edefe026ddc8403fcbc5cb2788d50e1b9942223d
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2011-07-23 11:24:58 -0400

    removed the hash testing code from main controller

--- jsafe/src/main/java/chotchki/web/MainController.java
+++ jsafe/src/main/java/chotchki/web/MainController.java
@@ -1,27 +1,17 @@
 package chotchki.web;
 
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.sql.Connection;
-
-import javax.crypto.KeyGenerator;
-import javax.crypto.Mac;
-import javax.crypto.SecretKey;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import chotchki.security.SHA512PasswordEncoding;
-
 @Controller
 public class MainController {
 	protected Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@RequestMapping("/")
-	public String showIndexPage(Model mod) throws NoSuchAlgorithmException, InvalidKeyException{
+	public String showIndexPage(Model mod){
 		return "index";
 	}
 }
