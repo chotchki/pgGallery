2dab3b7804fdbb303a02effaba83733161003326
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2011-08-21 10:36:52 -0400

    quoting more columns due to mixed case

--- jsafe/src/main/java/chotchki/db/dao/SiteSettingsMapper.java
+++ jsafe/src/main/java/chotchki/db/dao/SiteSettingsMapper.java
@@ -8,12 +8,12 @@ import chotchki.db.pojo.SiteSettings;
 
 public interface SiteSettingsMapper {
 
-	@Select("select * from siteSettings where id = 1")
+	@Select("select * from \"siteSettings\" where id = 1")
 	public SiteSettings get();
 	
-	@Insert("insert into siteSettings(id, itemsPerPage, thumbHeight, thumbWidth, mainHeight, mainWidth) values(#{id}, #{itemsPerPage}, #{thumbHeight}, #{thumbWidth}, #{mainHeight}, #{mainWidth})")
+	@Insert("insert into \"siteSettings\"(id, \"itemsPerPage\", \"thumbHeight\", \"thumbWidth\", \"mainHeight\", \"mainWidth\") values(#{id}, #{itemsPerPage}, #{thumbHeight}, #{thumbWidth}, #{mainHeight}, #{mainWidth})")
 	public SiteSettings create(SiteSettings settings);
 	
-	@Update("update siteSettings set id = #{id}, itemsPerPage = #{itemsPerPage}, thumbHeight = #{thumbHeight}, thumbWidth = #{thumbWidth}, mainHeight = #{mainHeight}, mainWidth = #{mainWidth} where id = #{id}")
+	@Update("update \"siteSettings\" set id = #{id}, \"itemsPerPage\" = #{itemsPerPage}, \"thumbHeight\" = #{thumbHeight}, \"thumbWidth\" = #{thumbWidth}, \"mainHeight\" = #{mainHeight}, \"mainWidth\" = #{mainWidth} where id = #{id}")
 	public void update(SiteSettings settings);
 }
