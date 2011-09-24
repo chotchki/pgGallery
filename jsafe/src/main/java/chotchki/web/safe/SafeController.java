6e72fa273552d9b9195d70f12194ee0adb34a5cd
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2011-09-23 22:37:31 -0400

    adding to previous commit

---
+++ jsafe/src/main/java/chotchki/web/safe/SafeController.java
@@ -0,0 +1,10 @@
+package chotchki.web.safe;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/safe")
+public class SafeController {
+
+}
